ce3cc01e918aec1c9016ebffca62df8f8cc3a4d7
Jos van Bakel <jos@codeaddict.org>
2022-07-28 22:07:39 +0200

    Update vendorSha256

--- nix/pkgs/midimix.nix
+++ nix/pkgs/midimix.nix
@@ -9,7 +9,7 @@ buildGoModule rec {
   # go mod strips directories without packages. This strips parts of the gomidi
   # rtmidi cpp code.
   proxyVendor = true;
-  vendorSha256 = "sha256-Z3nSV2YN5zo3dszlw1MG5UPVNYoLvzy7crMR3QU82d0=";
+  vendorSha256 = "sha256-RVfx9+1KSfYYyeOX8V2O4zH9KWEiJ+V2GtGCQxE2A9c=";
 
   subPackages = [ "cmd/midimix" ];
 
